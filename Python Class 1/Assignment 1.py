afb824c863a912698f8a3ebb2efe3fdd2e6b807f
Gaurav-900 <worksforgauravsharma@gmail.com>
2026-06-06 22:23:42 +0530

    First assignment and ReadMe commit

---
+++ Python Class 1/Assignment 1.py
@@ -0,0 +1,11 @@
+#1) Practise print statement ─
+
+# multiple objects
+print("Hello", "World", 42, 3.14)
+ 
+# separator
+print("apple", "banana", "Mango", sep=" | ")
+ 
+# end
+print("wait", end=" ")
+print("")
---
+++ Python Class 1/Assignment 2.py
@@ -0,0 +1,4 @@
+#2) Greeting with user input ─
+ 
+name = input("Please enter your name: ")
+print("Hello", name, "have a great day")
